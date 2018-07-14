122e79e237bc0b4d5695b0cb22594929bf53858b
ipman98 <navilk@zoho.com>
2018-07-14 18:54:23 +0530

    root commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+$(window).on("load", function () {
+    getWordCount();
+    $('#inputWord').focus();
+    $('#inputWord').keyup(function () {
+        suggestWords();
+    });
+});
+
+function suggestWords() {
+    //suggest words on key up
+
+    var inputWord = $('#inputWord').val().trim();
+
+    if (!isEmpty(inputWord)) {
+        $('#listTitle').text("Suggestions");
+
+        //send get request & get words (0 response = null)
+        $.get('./php/suggest.php?s=' + inputWord, function (data) {
+            if (data !== '0') {
+                addToList(inputWord, data, 'suggest');
+                $('#welcomePanel').hide();
+                $('#list').fadeIn();
+
+            } else {
+                $("#listItems").empty();
+                $("#listItems").append('<a href="#" class="list-group-item sinhala">' + "Sorry!. '" + inputWord + "' is not included in our database at the moment." + '</a>');
+            }
+
+        });
+    } else {
+        $('#list').hide();
+        $('#welcomePanel').fadeIn();
+    }
+
+}
+
+function findMeaning(inputWord) {
+    $('#listTitle').text("Definitions");
+    $('#inputWord').val(inputWord);
+    //send get request & get meanings
+    $.get('./php/find.php?s=' + inputWord, function (data) {
+        addToList(inputWord, data, 'meaning');
+    });
+}
+
+function addToList(inputWord, data, type) {
+    //add meanings to div
+    $("#listItems").empty();
+    var jsonData = JSON.parse(data);
+    for (item in jsonData) {
+        if (type == 'suggest') {
+            $("#listItems").append('<a href="#" onclick="findMeaning(' + "'" + jsonData[item] + "'" + ')" class="list-group-item sinhala">' + jsonData[item] + '</a>');
+        } else {
+            $("#listItems").append('<a href="#" class="list-group-item sinhala">' + jsonData[item] + '</a>');
+        }
+    }
+}
+
+function getWordCount() {
+    $.get('./php/count.php', function (data) {
+        $('#wordCount').text(data);
+    });
+}
+
+function isEmpty(val) {
+    //check if input null or empty
+    if (val !== null && val !== '') {
+        return false;
+    } else {
+        return true;
+    }
+}
---
+++ php/config.php
@@ -0,0 +1,18 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'zigiri');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+//set charset
+$link->set_charset("utf8");
+
+// Check connection
+if($link === false){
+  die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+?>
